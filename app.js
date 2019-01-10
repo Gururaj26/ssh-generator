79abcb509e0ccbc10cb346aeb970b46b5532178f
Gururaj Dharani <dharani.gururaj@gmail.com>
2019-01-11 01:39:42 +0530

    add: exec command to create ssh key with confirmation

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 const inquirer = require('inquirer');
 const chalkPipe = require('chalk-pipe');
-
+const execSync = require('child_process').execSync;
 const utils = require('./utils.js');
 const questions = utils.questions;
 
 inquirer.prompt(questions).then(answers => {
   const command = 'ssh-keygen -t rsa -b 4096 -f ~/.ssh/' + answers.username + '-' + answers.domain + ' ' + '-C' + ' ' + '"' + answers.username + '-' + answers.domain + '"';
-  console.log('hery', JSON.stringify(answers, null, ' '));
-  console.log('Paste this command in terminal', command);
+  let cmd = execSync(command);
+  if(answers.isConfirm) console.log('Successfully created your ssh keys');
 });
