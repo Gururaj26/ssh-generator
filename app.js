0491b807d0d4dab6c7d9af2cb81750bc0c2052da
Gururaj Dharani <dharani.gururaj@gmail.com>
2019-01-30 22:47:10 +0530

    fix: ssh post generation prompt and color coded for just printing key

--- app.js
+++ app.js
@@ -1,23 +1,36 @@
 #! /usr/bin/env node
+
 const inquirer = require('inquirer')
 const chalkPipe = require('chalk-pipe')
-const execSync = require('child_process').execSync
 const utils = require('./utils.js')
 const questions = utils.questions
 
-function validateUrl(name){
+var exec = require('child_process').exec;
+
+
+function validateUrl(name) {
   const exp = /[-a-zA-Z0-9@:%_\+.~#?&//=]{2,256}\.[a-z]{2,4}\b(\/[-a-zA-Z0-9@:%_\+.~#?&//=]*)?/gi;
   const regex = new RegExp(exp);
   const isValid = name.match(regex);
   return (isValid ? true : false) || '~ Url is not valid'
 }
 
-function generateKeys(username, domain, type){
+function generateKeys(username, domain, type) {
   let exp = 'ssh-keygen -t rsa -b 4096 -f ~/.ssh/' + username + '-' + domain + ' ' + '-C' + ' ' + '"' + username + '-' + domain + '"';
-  if(type=='print'){
-    console.log('Paste this to create your ssh keys: ', exp);
+  let printExp = `${'cat ~/.ssh/' + username + '-' + domain + '.pub'}`;
+  if (type == 'print') {
+    console.log('Paste this to create your ssh keys: -------------------------------' + '\n' + '\n' , chalkPipe('orange')(exp));
   } else {
-    execSync(exp);
+    let child = exec(exp);
+    child.stdout.on('data', function(data) {
+      console.log('stdout: ' + data);
+    });
+    child.stderr.on('data', function(data) {
+      console.log('stdout: ' + data);
+    });
+    child.on('close', function(code) {
+      console.log('closing code: ' + code);
+    });
   }
 }
 
@@ -31,14 +44,14 @@ inquirer.prompt(questions).then(answers => {
       message: 'Enter the Custom Domain Name :',
       validate: validateUrl
     }).then(ans => {
-        if(answers.isConfirm){
-          generateKeys(answers.username, ans.domain_name, 'generate');
-        } else {
-          generateKeys(answers.username, ans.domain_name, 'print');
-        }
+      if (answers.isConfirm) {
+        generateKeys(answers.username, ans.domain_name, 'generate');
+      } else {
+        generateKeys(answers.username, ans.domain_name, 'print');
+      }
     })
   }
-  if(answers.domain != 'custom_domain'){
+  if (answers.domain != 'custom_domain') {
     if (answers.isConfirm) {
       generateKeys(answers.username, domain, 'generate');
     } else {
